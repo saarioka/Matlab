952e0e4cf7cd6f02f88edf1c94f6e372693a06fa
Santeri Yoga <santeri.saariokari@gmail.com>
2018-02-10 21:49:37 +0200

    termo

---
+++ maxwellConstruction.m
@@ -0,0 +1,54 @@
+% Termo LH10 teht‰v‰ 3 - Santeri Saariokari
+clear all
+syms t k
+format short
+figure
+% Piirret‰‰n kuva funktiosta
+v=linspace(0.6,2.5);
+p=(8*0.95)./(3.*v-1)-3./(v.^2);
+plot(v,p)
+axis([0 2.5 0.6 0.9])
+
+% Funktiona
+pf(t)=(8*0.95)./(3.*t-1)-3./(t.^2);
+
+% Muodostetaan kolme x:n arvoa jolla saadaan sama y kuin muttujalla alaraja
+% L‰hdet‰‰n liikkeelle arvauksella 0.68
+alaraja=0.68;
+assume(k>0.65);
+assume(k<3);
+x=double(solve((8*0.95)./(3.*k-1)-3./(k.^2)-pf(alaraja)));
+
+% Ne ovat nyt vektorissa x:talletetaan ne muuttujiin
+keski=x(2);
+ylaraja=x(3);
+
+% Ensimm‰inen pinta-ala on suorakaide - integraali
+A1=(keski-alaraja)*pf(alaraja)-double(int(pf,alaraja,keski));
+
+% Toinen pinta-ala on integraali - suorakaide
+A2=double(int(pf,keski,ylaraja))-(ylaraja-keski)*pf(keski);
+
+% Etsit‰‰n nyt numeerisesti raja, jolloin A1=A2, ts. A1-A2=0 (l‰hes)
+tic
+while(A1-A2)>0,
+    alaraja=alaraja+0.0001;
+    A1=abs(double(int(pf,alaraja,keski))-(keski-alaraja)*pf(alaraja));
+    A2=double(int(pf,keski,ylaraja))-(ylaraja-keski)*pf(keski);
+    x=double(solve((8*0.95)./(3.*k-1)-3./(k.^2)-pf(alaraja)));
+    keski=x(2);
+    ylaraja=x(3);
+end
+toc
+% Piirret‰‰n apuviiva ja -pisteet
+hold on;
+plot(x,pf(x),'r*')
+line([0 2.5],[pf(alaraja) pf(alaraja)]);
+
+disp('Tulokset:')
+disp('Eka ala')
+disp(double(A1))
+disp('Toka ala')
+disp(double(A2))
+disp('Paine')
+disp(double(pf(alaraja)))
---
+++ vapausasteet.m
@@ -0,0 +1,21 @@
+Omega_id=[];
+for i=0:10
+    Omega_id=[Omega_id nchoosek(i+3-1,i)];
+end
+Omega_id=Omega_id.';
+kvantit=[0 1 2 3 4 5 6 7 8 9 10].';
+
+Omega_unid=[1 1 2 3 4 5 6 8 9 11 14].';
+
+table(Kvantit,Omega_id,Omega_unid)
+
+plot(Kvantit,Omega_id)
+title('Identifioidut vapausasteet kvanttien määrän funktiona')
+xlabel('kvantit')
+ylabel('vapausasteet')
+
+figure
+plot(kvantit,Omega_unid)
+title('Identifioitumattomat vapausasteet kvanttien määrän funktiona')
+xlabel('kvantit')
+ylabel('vapausasteet')
