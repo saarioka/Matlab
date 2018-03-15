f2d315d2c4c5645bed2b27ae2a1f64bae2a89792
saarioka <34010391+saarioka@users.noreply.github.com>
2018-03-15 22:07:00 +0200

    kakkone

--- Sci_comp/E2/normalize_cols.m
+++ Sci_comp/E2/normalize_cols.m
@@ -1,5 +1,6 @@
 function A = normalize_cols(A, p)
-    
+% NORMALIZE_COLS  divides each column of A by its p-norm
+
     if nargin == 1
         p = 2;
     end
---
+++ Sci_comp/E2/sqrt_square.m
@@ -0,0 +1,17 @@
+function x = sqrt_square(x, n)
+% SQRT_SQUARE takes square root of x n times and
+% squares it n times
+
+    assert(all(all(x>=0)>=0), 'x must be positive')
+    
+    assert(round(n) == n, 'n must be an integer')
+    
+    for root = 1:n
+        x = sqrt(x);
+    end
+    
+    for exp = 1:n
+        x = x.^2;
+    end
+    
+end
